f69138fa4ee40583df183ee3c0e37591c62dc1df
Paul Talbot <15139535+pvtalbot@users.noreply.github.com>
2026-07-11 16:53:46 +0200

    feat: Axum & DB connection, and switch to english

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,31 +1,43 @@
-use axum::{Router, routing::get};
+use axum::{Router, extract::State, routing::get};
 use sqlx::PgPool;
-
-
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
     let database_url = std::env::var("DATABASE_URL")
-        .expect("La variable DATABASE_URL doit être définie dans le fichier .env");
+        .expect("DATABASE_URL variable must be set in the .env file");
 
     let pool = PgPool::connect(&database_url)
         .await
-        .expect("Impossible de se connecter à la base de données");
+        .expect("Failed to connect to the database");
 
-    println!("DB connectée");
+    println!("✅ Database connected successfully!");
 
-    let app: Router<()> = Router::new().route("/",
-        get(|| async { "Bienvenue sur l'API de compta !"})
-    );
+    // The Router automatically infers Router<PgPool> thanks to .with_state(pool)
+    let app = Router::new()
+        .route("/", get(welcome))
+        .route("/status", get(check_db_status))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await 
-        .unwrap()
-    ;
+        .await
+        .unwrap();
 
-    println!("Serveur démarré");
+    println!("🚀 Server started on http://127.0.0.1:3000");
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn welcome() -> &'static str {
+    "Welcome to the Accounting API!"
+}
+
+async fn check_db_status(State(pool): State<PgPool>) -> String {
+    let version: (String,) = sqlx::query_as("SELECT version()")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    format!("Database is responding! Postgres version: {}", version.0)
 }
